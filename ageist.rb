4e9edb7e1c5c6f5e7bb54038190d2a9a420e2bda
manzosk3 (Luis Manzo) <manzosk3@gmail.com>
2014-10-29 10:06:25 -0700

    complete ageist

---
+++ ageist.rb
@@ -0,0 +1,41 @@
+
+
+age = rand(111)
+puts "Age is #{age} where #{age} is a random number between 0 and 110"
+
+
+  # if age <= 1
+  #   puts 'baby'
+  # elsif age > 1 && age <= 9
+  #   puts 'child'
+  # elsif age >= 10 && age <= 12
+  #   puts 'tween'
+  # elsif age >= 13 && age <= 19
+  #     puts 'teenager'
+  #   elsif age >= 20 && age <= 40
+  #       puts 'adult'
+  #     elsif age >= 41 && age <= 65
+  #         puts 'middle age'
+  #       elsif age >= 66  && age <= 100
+  #           puts 'senior'
+  #         elsif age > 100
+  #               puts 'record breaking'
+  #   end
+
+
+# case
+# when age < 1
+#   puts 'baby'
+# when  age> 1 &&  age<= 9
+#   puts 'child'
+# when  age > 10 &&  age<= 19
+#   puts 'child'
+# when  age > 20 &&   age<= 40
+#   puts 'tween'
+# when  age > 41 &&  age <= 65
+#   puts 'adult'
+# when  age > 66 &&  age <= 100
+#   puts 'senior'
+# when  age > 100
+#   puts 'record breaking'
+# end
